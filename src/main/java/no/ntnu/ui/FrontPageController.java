3b4e4a07b9f855e833d0262b293f161863913f3a
Filip Berg Vindsetmo <filip@vindsetmo.no>
2022-12-05 18:57:07 +0100

    Further work on GUI

--- src/main/java/no/ntnu/ui/FrontPageController.java
+++ src/main/java/no/ntnu/ui/FrontPageController.java
@@ -1,5 +1,29 @@
 package no.ntnu.ui;
 
-public class FrontPageController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class FrontPageController implements Initializable {
+
+    @FXML
+    private LineChart< ? , ? > chart;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        XYChart.Series series = new XYChart.Series();
+
+        series.getData().add(new XYChart.Data("1",4));
+        series.getData().add(new XYChart.Data("2",2));
+        series.getData().add(new XYChart.Data("3",3));
+
+        chart.getData().addAll(series);
+
+
+    }
 
 }
